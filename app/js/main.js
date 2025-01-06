d41164a388f131a6e04d3f2c46fbc66cef010360
Krasnov91 <krasnoshtanov10.11.91@gmail.com>
2025-01-06 22:44:25 +0100

    added section video, snippet fancybox

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+
 $(function(){
 
   $('.top-slider__inner').slick ({
@@ -8,3 +9,15 @@ $(function(){
     autoplaySpeed: 2000
   });
 });
+
+// let a = '5';
+// let b = 5;
+// let c = false;
+
+// // c = b / a;
+
+// console.log(c);
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
